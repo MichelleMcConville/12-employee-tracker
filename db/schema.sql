e1f1d8515b9e72a7452dc7d05ffbad87882d8d2d
Michelle McConville <mchelmconvl@gmail.com>
2020-11-26 15:33:06 -0600

    moved FK & REF to associated line

--- db/schema.sql
+++ db/schema.sql
@@ -12,16 +12,13 @@ CREATE TABLE employee_role (
   id INT NOT NULL auto_increment PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT, 
-  FOREIGN KEY (department_id) REFERENCES department(id)
+  department_id INT, FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
   id INT NOT NULL auto_increment PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT,
-  manager_id INT,
-  FOREIGN KEY (role_id) REFERENCES employee_role(id),
-  FOREIGN KEY (manager_id) REFERENCES employee(id)
+  role_id INT, FOREIGN KEY (role_id) REFERENCES employee_role(id),
+  manager_id INT, FOREIGN KEY (manager_id) REFERENCES employee(id)
 );

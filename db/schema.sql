4743d7c1f82f2330d6626c09b90e1017238ba030
Michelle McConville <mchelmconvl@gmail.com>
2020-11-26 15:17:22 -0600

    added employee table

--- db/schema.sql
+++ db/schema.sql
@@ -12,16 +12,16 @@ CREATE TABLE employee_role (
   id INT NOT NULL auto_increment PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT,
-  FOREIGN KEY (department_id) REFERENCES (department_id)
+  department_id INT, 
+  FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
   id INT NOT NULL auto_increment PRIMARY KEY,
-  first_name
-  last_name
-  role_id
-  manager_id
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT,
+  FOREIGN KEY (role_id) REFERENCES employee_role(id),
+  FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
-
-

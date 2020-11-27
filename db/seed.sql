de565bf94024f32cbbd2e5af0a0f3219df552673
Michelle McConville <mchelmconvl@gmail.com>
2020-11-27 16:05:30 -0600

    added role seed info

--- db/seed.sql
+++ db/seed.sql
@@ -8,7 +8,14 @@ VALUES
 
 INSERT INTO employee_role (title, salary, department_id)
 VALUES
-  (''),
+  ('Founder',             '10000',  '01'),
+  ('Headmaster',          '200000', '05'),
+  ('Headmistress',        '200000', '05'),
+  ('Deputy Headmaster',   '135000', '15'),
+  ('Deputy Headmistress', '135000', '15'),
+  ('Teacher',             '80000',  '20'),
+  ('Student',             '0',      '77'),
+  ('Ghosts',              '0',      '13');
 
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
 VALUES

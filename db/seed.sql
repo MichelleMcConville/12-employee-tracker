85069334ed176c6bc28963acf4b5590ee9f29461
Michelle McConville <mchelmconvl@gmail.com>
2020-11-27 11:23:15 -0600

    starter seed code

--- db/seed.sql
+++ db/seed.sql
@@ -1,1 +1,12 @@
-x
+INSERT INTO department (dept_name)
+VALUES
+  (''),
+
+INSERT INTO employee_role (title, salary, department_id)
+VALUES
+  (''),
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+  (''),
+  

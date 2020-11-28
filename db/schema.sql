cdbc17c2042a6154427667e1ce7518a4ed23e6ee
Michelle McConville <mchelmconvl@gmail.com>
2020-11-28 15:00:48 -0600

    rearranged inserts4

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,7 @@
-DROP DATABASE IF EXISTS employeeRoster_db;
-CREATE DATABASE employeeRoster_db;
+DROP DATABASE IF EXISTS employee_roster_db;
+CREATE DATABASE employee_roster_db;
 
-USE employeeRoster_db;
+USE employee_roster_db;
 
 CREATE TABLE department (
   id INT NOT NULL auto_increment PRIMARY KEY,
@@ -12,13 +12,13 @@ CREATE TABLE employee_role (
   id INT NOT NULL auto_increment PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT, FOREIGN KEY (department_id) REFERENCES department(id)
+  department_id INT, CONSTRAINT fkDept_id FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
   id INT NOT NULL auto_increment PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT, FOREIGN KEY (role_id) REFERENCES employee_role(id),
-  manager_id INT, FOREIGN KEY (manager_id) REFERENCES employee(id)
+  role_id INT, CONSTRAINT fkRole_id FOREIGN KEY (role_id) REFERENCES employee_role(id),
+  manager_id INT, CONSTRAINT fkMgr_id FOREIGN KEY (manager_id) REFERENCES employee(id)
 );

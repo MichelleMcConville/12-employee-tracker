0213798e49f10c71e47db599e2278963e44e4728
Michelle McConville <mchelmconvl@gmail.com>
2020-12-01 14:34:02 -0600

    updated db name & dept_id ref

--- db/schema.sql
+++ db/schema.sql
@@ -1,24 +1,24 @@
-DROP DATABASE IF EXISTS employee_roster_db;
-CREATE DATABASE employee_roster_db;
+DROP DATABASE IF EXISTS employee_roster;
+CREATE DATABASE employee_roster;
 
-USE employee_roster_db;
+USE employee_roster;
 
 CREATE TABLE department (
   id INT NOT NULL auto_increment PRIMARY KEY,
   dept_name VARCHAR(30)
 );
 
-CREATE TABLE employee_role (
+CREATE TABLE role (
   id INT NOT NULL auto_increment PRIMARY KEY,
   title VARCHAR(30),
   salary DECIMAL,
-  department_id INT, CONSTRAINT fkDept_id FOREIGN KEY (department_id) REFERENCES department(id)
+  dept_id INT, CONSTRAINT fkDept_id FOREIGN KEY (dept_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
   id INT NOT NULL auto_increment PRIMARY KEY,
   first_name VARCHAR(30),
   last_name VARCHAR(30),
-  role_id INT, CONSTRAINT fkRole_id FOREIGN KEY (role_id) REFERENCES employee_role(id),
+  role_id INT, CONSTRAINT fkRole_id FOREIGN KEY (role_id) REFERENCES role(id),
   manager_id INT, CONSTRAINT fkMgr_id FOREIGN KEY (manager_id) REFERENCES employee(id)
 );

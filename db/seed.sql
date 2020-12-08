cc554733fea17d547b0b37424133b022f23f2135
Michelle McConville <mchelmconvl@gmail.com>
2020-12-08 17:01:35 -0600

    updated seed data

--- db/seed.sql
+++ db/seed.sql
@@ -2,41 +2,41 @@ INSERT INTO departments (dept_name)
 VALUES
   ('None'),
   ('IWC'),
-  ('Hogwarts'),
-  ('Gryffindor'),
-  ('Hufflepuff'),
-  ('Ravenclaw'),
-  ('Slytherin'),
-  ('Alumni');
+  ('Executive'),
+  ('Education'),
+  ('Support'),
+  ('Scholarship'),
+  ('Pupil'),
+  ('Alumni'),
+  ('Deceased');
 
 INSERT INTO roles (title, salary, dept_id)
 VALUES
   ('None',                0,      1),
-  ('Founder',             1000,   2),
-  ('Headmaster',          200000, 3),
-  ('Deputy',              135000, 3),
-  ('Head of House',       120000, null),
-  ('Teaching Staff',      80000,  3),
-  ('Support Staff',       50000,  3),
-  ('Student',             0,      null),
-  ('Ghosts',              0,      null),
-  ('Alumni',              0,      8);
-
+  ('Headmaster',          200000, 2),
+  ('Deputy',              150000, 3),
+  ('Head of House',       120000, 3),
+  ('Professor',           100000, 4),
+  ('Support Staff',       50000,  5),
+  ('Prefect',             10000,  6),
+  ('Student',             0,      7),
+  ('Alumni',              0,      8),
+  ('Ghosts',              0,      9);
 
 INSERT INTO employees (first_name, last_name, role_id, manager_id)
 VALUES
-  ('No',              'Manager',      1,  null),
-  ('Godrick',         'Gryffindor',   2,  null),
-  ('Albus',           'Dumbledore',   3,  null),
-  ('Minerva',         'McGonagall',   5,  3),
-  ('Severus',         'Snape',        5,  3),
-  ('Filius',          'Flitwick',     5,  3),
-  ('Pomona',          'Sprout',       5,  3),
-  ('Rubeus',          'Hagrid',       6,  3),
-  ('Argus',           'Filtch',       7,  3),
-  ('Hermione',        'Granger',      8,  4),
-  ('Cedric',          'Diggory',      8,  6),
-  ('Luna',            'Lovegood',     8,  7),
-  ('Draco',           'Malfoy',       8,  5),
-  ('Nearly Headless', 'Nick',         9,  null),
-  ('Sirius',          'Black',       10,  null);
+  ('No',              'Manager',      1,  1),
+  ('Albus',           'Dumbledore',   2,  1),
+  ('Minerva',         'McGonagall',   3,  2),
+  ('Severus',         'Snape',        4,  2),
+  ('Filius',          'Flitwick',     4,  2),
+  ('Pomona',          'Sprout',       4,  2),
+  ('Rubeus',          'Hagrid',       5,  2),
+  ('Argus',           'Filtch',       6,  2),
+  ('Ron',             'Weasley',      7,  3),
+  ('Hermione',        'Granger',      8,  3),
+  ('Cedric',          'Diggory',      8,  5),
+  ('Luna',            'Lovegood',     8,  6),
+  ('Draco',           'Malfoy',       8,  4),
+  ('Nearly Headless', 'Nick',        10,  1),
+  ('Sirius',          'Black',        9,  1);
